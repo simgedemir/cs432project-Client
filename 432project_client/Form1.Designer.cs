41f981dfb664d7425376b655324047fce624cab1
unknown <simgedemir@sabanciuniv.edu>
2019-03-28 18:59:55 +0300

    enrollment phase for client(to be checked)

--- 432project_client/Form1.Designer.cs
+++ 432project_client/Form1.Designer.cs
@@ -45,83 +45,83 @@
             // 
             // passwordBox
             // 
-            this.passwordBox.Location = new System.Drawing.Point(78, 152);
-            this.passwordBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.passwordBox.Location = new System.Drawing.Point(156, 292);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordBox.Name = "passwordBox";
-            this.passwordBox.Size = new System.Drawing.Size(95, 20);
+            this.passwordBox.Size = new System.Drawing.Size(186, 31);
             this.passwordBox.TabIndex = 0;
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(78, 114);
-            this.usernameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.usernameBox.Location = new System.Drawing.Point(156, 219);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(95, 20);
+            this.usernameBox.Size = new System.Drawing.Size(186, 31);
             this.usernameBox.TabIndex = 1;
             this.usernameBox.TextChanged += new System.EventHandler(this.usernameBox_TextChanged);
             // 
             // ipBox
             // 
-            this.ipBox.Location = new System.Drawing.Point(78, 44);
-            this.ipBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ipBox.Location = new System.Drawing.Point(156, 85);
+            this.ipBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ipBox.Name = "ipBox";
-            this.ipBox.Size = new System.Drawing.Size(95, 20);
+            this.ipBox.Size = new System.Drawing.Size(186, 31);
             this.ipBox.TabIndex = 2;
             // 
             // portBox
             // 
-            this.portBox.Location = new System.Drawing.Point(78, 78);
-            this.portBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.portBox.Location = new System.Drawing.Point(156, 150);
+            this.portBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.portBox.Name = "portBox";
-            this.portBox.Size = new System.Drawing.Size(95, 20);
+            this.portBox.Size = new System.Drawing.Size(186, 31);
             this.portBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 117);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(34, 225);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Username:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 153);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(36, 294);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(112, 25);
             this.label2.TabIndex = 5;
             this.label2.Text = "Password:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(54, 47);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(108, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.Size = new System.Drawing.Size(37, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "IP:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 81);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(90, 156);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.Size = new System.Drawing.Size(57, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Port:";
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(30, 196);
-            this.connectButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.connectButton.Location = new System.Drawing.Point(60, 377);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(72, 30);
+            this.connectButton.Size = new System.Drawing.Size(144, 58);
             this.connectButton.TabIndex = 9;
             this.connectButton.Text = "Connect";
             this.connectButton.UseVisualStyleBackColor = true;
@@ -129,28 +129,29 @@
             // 
             // disconnectButton
             // 
-            this.disconnectButton.Location = new System.Drawing.Point(112, 196);
-            this.disconnectButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.disconnectButton.Location = new System.Drawing.Point(224, 377);
+            this.disconnectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.disconnectButton.Name = "disconnectButton";
-            this.disconnectButton.Size = new System.Drawing.Size(73, 30);
+            this.disconnectButton.Size = new System.Drawing.Size(146, 58);
             this.disconnectButton.TabIndex = 10;
             this.disconnectButton.Text = "Disconnect";
             this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
             // 
             // logs
             // 
-            this.logs.Location = new System.Drawing.Point(212, 35);
-            this.logs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.logs.Location = new System.Drawing.Point(424, 67);
+            this.logs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.logs.Name = "logs";
-            this.logs.Size = new System.Drawing.Size(152, 245);
+            this.logs.Size = new System.Drawing.Size(300, 467);
             this.logs.TabIndex = 11;
             this.logs.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 291);
+            this.ClientSize = new System.Drawing.Size(762, 560);
             this.Controls.Add(this.logs);
             this.Controls.Add(this.disconnectButton);
             this.Controls.Add(this.connectButton);
@@ -162,7 +163,7 @@
             this.Controls.Add(this.ipBox);
             this.Controls.Add(this.usernameBox);
             this.Controls.Add(this.passwordBox);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- 432project_client/Form1.cs
+++ 432project_client/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Threading;
-
+using System.Security.Cryptography;
 
 namespace _432project_client
 {
@@ -44,13 +44,20 @@ namespace _432project_client
 
         private void connectButton_Click(object sender, EventArgs e)
         {
+            string enc_dec_keys, sig_ver_keys;
+            using (System.IO.StreamReader fileReader =
+            new System.IO.StreamReader("server_enc_dec_pub.txt"))
+            {
+                enc_dec_keys = fileReader.ReadLine();
+            }
+   
             clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            //comment
             string IP = ipBox.Text;
             int port;
             string username = usernameBox.Text;
             string password = passwordBox.Text;
+            Byte[] buffer = new Byte[256];
             if (Int32.TryParse(portBox.Text, out port))
             {
                 try
@@ -59,7 +66,62 @@ namespace _432project_client
                     connectButton.Enabled = false;
                     connected = true;
                     logs.AppendText("Connected to server\n");
-                    //username & password send
+                    //hashing the password
+                    byte[] hashedPass = hashWithSHA256(password);
+                    byte[] halfPass = new byte[16];
+                    Array.Copy(hashedPass, halfPass, 16);
+                    //concatenating password and username 
+                    string message= generateHexStringFromByteArray(halfPass) + username;
+                    //RSA encryption
+                    byte[] encrptedRSAmessage = encryptWithRSA(message, 3072, enc_dec_keys);
+                    //sending to the server
+                    try
+                    {
+                        clientSocket.Send(encrptedRSAmessage);
+                        logs.AppendText("Encrypted message has been sent. \n");
+                    }
+                    catch
+                    {
+                        connected = false;
+                        logs.AppendText("Server is down. Your message could not be sent.\n");
+                        clientSocket.Close();
+                    }
+                    /*
+                    try
+                    {
+                        clientSocket.Receive(buffer);
+                        string incomingMessage= Encoding.Default.GetString(buffer); 
+                        // signing with RSA 4096
+                        byte[] signatureRSA = signWithRSA(incomingMessage, 3072, sig_ver_keys);
+                        // verifying with RSA 4096
+                        bool verificationResult = verifyWithRSA(incomingMessage, 3072, sig_ver_keys, signatureRSA);
+                        if (verificationResult == true)
+                        {
+                            logs.AppendText("Valid signature \n");
+                            if (incomingMessage.Contains("Success"))
+                            {
+                                logs.AppendText("Enrollments is successful.\n");
+                            }
+                            else
+                            {
+                                logs.AppendText("Try with another username.\n");
+                                connected = false;
+                                clientSocket.Close();
+                            }
+                        }
+                        else
+                        {
+                            logs.AppendText("Invalid signature \n");
+                            // What should we do???
+                        }
+                        
+                    }
+                    catch
+                    {
+                        connected = false;
+                        logs.AppendText("Server is down.\n");
+                        clientSocket.Close();
+                    } */
                     Thread receiveThread = new Thread(new ThreadStart(Receive));
                     receiveThread.Start();
 
@@ -73,19 +135,52 @@ namespace _432project_client
             {
                 logs.AppendText("Check the port\n");
             }
+
+            
+
         }
         private void Receive()
         {
+            string sig_ver_keys;
+            using (System.IO.StreamReader fileReader =
+            new System.IO.StreamReader("server_signing_verification_pub.txt"))
+            {
+                 sig_ver_keys = fileReader.ReadLine();
+            }
+
             while (connected)
             {
                 try
                 {
-                    Byte[] buffer = new Byte[64];
+                    Byte[] buffer = new Byte[256];
                     clientSocket.Receive(buffer);
 
                     string incomingMessage = Encoding.Default.GetString(buffer);
                     incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
-                    logs.AppendText(incomingMessage + "\n");
+                    // signing with RSA 4096
+                    byte[] signatureRSA = signWithRSA(incomingMessage, 3072, sig_ver_keys);
+                    // verifying with RSA 4096
+                    bool verificationResult = verifyWithRSA(incomingMessage, 3072, sig_ver_keys, signatureRSA);
+                    if (verificationResult == true)
+                    {
+                        logs.AppendText("Valid signature \n");
+                        if (incomingMessage.Contains("Success"))
+                        {
+                            logs.AppendText("Enrollments is successful.\n");
+                        }
+                        else
+                        {
+                            logs.AppendText("Try with another username.\n");
+                            connected = false;
+                            clientSocket.Close();
+                        }
+                    }
+                    else
+                    {
+                        logs.AppendText("Invalid signature \n");
+                        // What should we do???
+                    }
+                    //logs.AppendText(incomingMessage + "\n");
                 }
                 catch
                 {
@@ -98,6 +193,111 @@ namespace _432project_client
                     connected = false;
                 }
             }
+        }
+
+        // signing with RSA
+        static byte[] signWithRSA(string input, int algoLength, string xmlString)
+        {
+            // convert input string to byte array
+            byte[] byteInput = Encoding.Default.GetBytes(input);
+            // create RSA object from System.Security.Cryptography
+            RSACryptoServiceProvider rsaObject = new RSACryptoServiceProvider(algoLength);
+            // set RSA object with xml string
+            rsaObject.FromXmlString(xmlString);
+            byte[] result = null;
+
+            try
+            {
+                result = rsaObject.SignData(byteInput, "SHA256");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return result;
+        }
+
+        // verifying with RSA
+        static bool verifyWithRSA(string input, int algoLength, string xmlString, byte[] signature)
+        {
+            // convert input string to byte array
+            byte[] byteInput = Encoding.Default.GetBytes(input);
+            // create RSA object from System.Security.Cryptography
+            RSACryptoServiceProvider rsaObject = new RSACryptoServiceProvider(algoLength);
+            // set RSA object with xml string
+            rsaObject.FromXmlString(xmlString);
+            bool result = false;
+
+            try
+            {
+                result = rsaObject.VerifyData(byteInput, "SHA256", signature);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return result;
+        }
+        private Exception Exception()
+        {
+            throw new NotImplementedException();
+        }
+        // RSA encryption with varying bit length
+        static byte[] encryptWithRSA(string input, int algoLength, string xmlStringKey)
+        {
+            // convert input string to byte array
+            byte[] byteInput = Encoding.Default.GetBytes(input);
+            // create RSA object from System.Security.Cryptography
+            RSACryptoServiceProvider rsaObject = new RSACryptoServiceProvider(algoLength);
+            // set RSA object with xml string
+            rsaObject.FromXmlString(xmlStringKey);
+            byte[] result = null;
+
+            try
+            {
+                //true flag is set to perform direct RSA encryption using OAEP padding
+                result = rsaObject.Encrypt(byteInput, true);
+            }
+            catch 
+            {
+                //logs.AppendText("Encryption could not be done. \n");
+            }
+
+            return result;
+        }
+        public static string generateHexStringFromByteArray(byte[] input)
+        {
+            string hexString = BitConverter.ToString(input);
+            return hexString.Replace("-", "");
+        }
+
+        public static byte[] hexStringToByteArray(string hex)
+        {
+            int numberChars = hex.Length;
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+        static byte[] hashWithSHA256(string input)
+        {
+            // convert input string to byte array
+            byte[] byteInput = Encoding.Default.GetBytes(input);
+            // create a hasher object from System.Security.Cryptography
+            SHA256CryptoServiceProvider sha256Hasher = new SHA256CryptoServiceProvider();
+            // hash and save the resulting byte array
+            byte[] result = sha256Hasher.ComputeHash(byteInput);
+
+            return result;
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            logs.AppendText("Disconnected...\n");
+            connected = false;
+            clientSocket.Close();
         }
     }
 }
